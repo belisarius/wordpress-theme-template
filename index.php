0b0860c2e5761962455e34bcad0d3ac1f70a0a89
Ben <me+git@benspants.com>
2013-07-08 23:54:10 -0700

    Adding scss files and fire.app configuration. Added .gitignore and a few entries to said file.

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php // index.php ?>
+
+<?php get_header(); // Includes the content of header.php ?>
+            <?php 
+            // This is the beginning of the Loop.
+            if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+            <div class="l-main-content">
+            <h1 class="post-headline"><?php the_title(); ?></h1>
+            <div class="post-meta">
+            	<?php // This section is for things like number of comments, date posted, author, etc. ?>
+            </div>
+              <?php the_content(); // Echoes the content of whatever post we're looping through. ?>
+          </div>
+
+            <?php 
+            // This is where the Loop ends.
+            endwhile; else: ?>
+                <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+<?php get_footer(); //Includes the content of footer.php ?>
